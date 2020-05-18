adbdac4b91eafe383520a14de7d2acca99473057
IvanLokhno <62656007+IvanLokhno@users.noreply.github.com>
2020-05-19 01:15:05 +0200

    Update ss.h

--- ss.h
+++ ss.h
@@ -1,26 +1,26 @@
-using namespace std;
+using namespace std; 
  
 template < class T, int max_size >
 class Stack
 {
-         T            s [max_size];
-         int          top;
+         T s [max_size];
+         int top;
 public:
-                      Stack () { top = 0; }
-         void         reset () { top = 0; }
-         void         push ( T i );
-         T            pop ();
-         bool         is_empty () { return top == 0; }
-         bool         is_full  () { return top == max_size; }
+         Stack () { top = 0; }
+         void reset () { top = 0; }
+         void push ( T i );
+         T pop ();
+         bool is_empty () { return top == 0; }
+         bool is_full  () { return top == max_size; }
 };
  
 /////////////////////////  Класс Poliz  /////////////////////////////
  
 class Poliz
 {
-         Lex        * p;
-         int          size;
-         int          free;
+         Lex * p;
+         int size;
+         int free;
 public:
     Poliz (int max_size)
     {
@@ -33,10 +33,10 @@ public:
         p [free] = l;
         free++;
     }
-    void         put_lex ( Lex l, int place) { p [place] = l; }
-    void         blank    () { free++; }
-    int          get_free () { return free; }
-    Lex        & operator[] (int index) 
+    void put_lex ( Lex l, int place) { p [place] = l; }
+    void blank    () { free++; }
+    int get_free () { return free; }
+    Lex & operator[] (int index) 
     {
         if (index > size)
             throw "POLIZ ERROR: out of array";
@@ -48,43 +48,47 @@ public:
                 return p[index];
         }
     }
-    void         print ()
+    void print ()
     {
+
+        cout << "--------------------------------" << endl;
+        cout << "Poliz:" << endl;
         for (int i = 0; i < free; ++i)
-            cout << p[i];
+            cout << i << ": " << p[i];
+        cout << "--------------------------------" << endl;
     }
 };
 /////////////////////////////////////////////////////////////////
 
 class Parser 
 {
-    Lex          curr_lex;
+    Lex curr_lex;
     type_of_lex  c_type;
     type_of_lex  tmp_type;
-    int          c_val;
+    int c_val;
     string       c_string_val;   
     Scanner      scan;
     Stack < int, 100 > st_int;
     Stack < type_of_lex, 100 >  st_lex;
-    void         P();
-    void         D1();
-    void         D();
-    void         I1();
-    void         CONST_();
-    void         S();
-    void         S1();
-    void         E();
-    void         E1();
-    void         T();
-    void         F();
-    void         dec ( type_of_lex type);
-    void         check_id ();
-    void         check_op ();
-    void         check_not ();
-    void         eq_type ();
-    void         eq_bool ();
-    void         check_id_in_read ();
-    void         gl ()
+    void P();
+    void D1();
+    void D();
+    void I1();
+    void CONST_();
+    void S();
+    void S1();
+    void E();
+    void E1();
+    void T();
+    void F();
+    void dec ( type_of_lex type);
+    void check_id ();
+    void check_op ();
+    void check_not ();
+    void eq_type ();
+    void eq_bool ();
+    void check_id_in_read ();
+    void gl ()
     {
         curr_lex = scan.get_lex();
         c_type = curr_lex.get_type();
