07b39375fc3b41449220611d3ec0ea4f3e2c5dc8
IvanLokhno <62656007+IvanLokhno@users.noreply.github.com>
2020-05-19 01:12:26 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,34 +1,39 @@
 #include "lexeme.cpp"
-#include "ss.cpp"
-
+#include "ss.cpp" 
+#include "execute.cpp"
 int 
 main (int argc, char* argv[])
 {
 	try
 	{
-		cout << "The lexical stage:" << endl;
-		cout.width(15);
+		// cout << "The lexical stage:" << endl;
+		cout.width(25);
 		cout << "_code_";
 		cout.width(20);
 		cout << "type_of_lex";
-		cout.width(10);
+		cout.width(25);
 		cout << "val";
 		cout.width(5);
 		cout << "str";
 		cout.width(5);
-		cout << "chr"<< endl;
-		Lex curr_lex;
-		Scanner scan (argv[1]);
-		curr_lex = scan.get_lex();
-		while( curr_lex.get_type() != 17 )
-		{
-			cout << endl;
-			curr_lex = scan.get_lex();
-		}
+		cout << "chr"<< endl << endl;
+		// Lex curr_lex;
+		// Scanner scan (argv[1]);
+		// curr_lex = scan.get_lex();
+		// while( curr_lex.get_type() != 17 )
+		// {
+		// 	cout << endl;
+		// 	curr_lex = scan.get_lex();
+		// }
 
-		cout << "The syntactic/semantic stage:" << endl;
-		Parser   pars(argv[1]);
-		pars.analyze ();
+		// cout << "The syntactic/semantic stage:" << endl;
+		// Parser   pars(argv[1]);
+		// pars.analyze ();
+
+		// cout << "\n EXECUTE:  " << endl;
+
+		Interpretator I (argv[1]);
+		I.interpretation ();
 		
 		return 0;
 	}
